e790889dea5786c7a1c84cdf093e7fb7519260e7
minhmoc <minhhn1093@gmail.com>
2018-08-27 22:01:16 -0500

    Fix python indentation

--- __init__.py
+++ __init__.py
@@ -8,21 +8,21 @@ LOGGER = getLogger(__name__)
 
 class EmotionSkill(MycroftSkill):
 
-    def __init__(self):
-        super(EmotionSkill, self).__init__(name="EmotionSkill")
+	def __init__(self):
+		super(EmotionSkill, self).__init__(name="EmotionSkill")
 
-    @intent_handler(IntentBuilder("ExtremelyHappy").require("Extremely").require("Happy"))
-    def handle_extremely_happy_intent(self, message):
-        self.speak_dialog("extremely.happy")
+	@intent_handler(IntentBuilder("ExtremelyHappy").require("Extremely").require("Happy"))
+	def handle_extremely_happy_intent(self, message):
+		self.speak_dialog("extremely.happy")
 
-    @intent_handler(IntentBuilder("Happy").require("Happy"))
-    def handle_happy_intent(self, message):
-        self.speak_dialog("happy")
+	@intent_handler(IntentBuilder("Happy").require("Happy"))
+	def handle_happy_intent(self, message):
+		self.speak_dialog("happy")
 
-    @intent_handler(IntentBuilder("Sad").require("Sad"))
-    def handle_sad_intent(self, message):
-        self.speak_dialog("sad")
+	@intent_handler(IntentBuilder("Sad").require("Sad"))
+	def handle_sad_intent(self, message):
+		self.speak_dialog("sad")
 
 def create_skill():
-    return EmotionSkill()
+	return EmotionSkill()
 
